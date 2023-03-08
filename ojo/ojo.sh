3739c3ff71ddd39aafbb1ca498be52f41aa8fcfa
Dwentzart <118625308+Dwentzart@users.noreply.github.com>
2023-03-08 08:47:41 +0700

    Update ojo.sh

--- ojo/ojo.sh
+++ ojo/ojo.sh
@@ -59,8 +59,7 @@ ojod config node tcp://localhost:${OJO_PORT}657
 ojod init $NODENAME --chain-id $OJO_CHAIN_ID
 
 # download genesis and addrbook
-wget -O genesis.json https://snapshots.nodeist.net/t/ojo/genesis.json --inet4-only
-mv genesis.json ~/.ojo/config
+curl -s https://rpc.devnet-n0.ojo-devnet.node.ojo.network/genesis | jq -r .result.genesis > $HOME/.ojo/config/genesis.json
 
 # set peers and seeds
 PEERS=5af3d50dcc231884f3d3da3e3caecb0deef1dc5b@142.132.134.112:25356,c0ee71c74858b339787320596b805ed631c48ebb@213.133.100.172:27433,affee2f485ca15c68c302ad98e8de41fcd0e71ba@162.19.238.49:26656,fbeb2b37fe139399d7513219e25afd9eb8f81f4f@65.21.170.3:38656,dc19e5d986ea79e70180cfbee7789de9cd79e14e@95.217.57.232:56656,97ff540b57b89dd0b6737eddb92977523dd5a7b3@195.3.221.58:12656,8a8b9a8a58c922a7693715100710697ec69b1478@65.109.92.235:11086,7416a65de3cc548a537dbb8bdf93dbd83fe401d2@78.107.234.44:26656
