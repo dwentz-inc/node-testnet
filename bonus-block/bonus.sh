2d4683d2fcffd738d7ee52d0ecfa4a92980f3d50
Dwentzart <118625308+Dwentzart@users.noreply.github.com>
2023-03-19 11:28:27 +0700

    Update bonus.sh

--- bonus-block/bonus.sh
+++ bonus-block/bonus.sh
@@ -67,6 +67,7 @@ sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.b
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${BONUS_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${BONUS_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${BONUS_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${BONUS_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${BONUS_PORT}660\"%" $HOME/.bonusblock/config/config.toml
 sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${BONUS_PORT}317\"%; s%^address = \":8080\"%address = \":${BONUS_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${BONUS_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${BONUS_PORT}091\"%" $HOME/.bonusblock/config/app.toml
 
+
 # config pruning
 pruning="custom"
 pruning_keep_recent="100"
@@ -84,7 +85,7 @@ sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0025ubonus\"/" $H
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.bonusblock/config/config.toml
 
 # reset
-bonus-blockd tendermint unsafe-reset-all --home $HOME/.bonusblock
+
 
 echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
 # create service
